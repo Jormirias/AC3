d4c65ac29df2829c3d976d2870680fb811f3cf90
Jorge Dias <jormirias@gmail.com>
2025-05-19 10:47:00 +0100

    decrypt added

--- tea.s
+++ tea.s
@@ -23,9 +23,9 @@ encrypt:
 
 	# loop variables
 	movl	$0, %r14d		# total sum
-	movl	$32, %r15d	# n = 32
+	movl	$0, %r15d	# n = 0
 
-	L1:
+	L1_loop:
 		# sum += delta;
 		addl	DELTA, %r14d
 
@@ -39,16 +39,16 @@ encrypt:
 		movl	%r9d, %ecx
 		addl	%r14d, %ecx
 
-		xorl	%ecx %ebx
+		xorl	%ecx, %ebx
 
 		# (z >> 5) + k[1]]
 		movl	%r9d, %edx
 		shrl	$5, %edx
 		addl	%r11d, %edx
 
-		xorl	%edx %ebx
+		xorl	%edx, %ebx
 
-		addl	%ebx %r8d
+		addl	%ebx, %r8d
 
 		# z += ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
 		# (y << 4) + k[2]
@@ -60,20 +60,21 @@ encrypt:
 		movl	%r8d, %ecx
 		addl	%r14d, %ecx
 
-		xorl	%ecx %ebx
+		xorl	%ecx, %ebx
 
 		# (y >> 5) + k[3]]
 		movl	%r8d, %edx
 		shrl	$5, %edx
 		addl	%r13d, %edx
 
-		xorl	%edx %ebx
+		xorl	%edx, %ebx
 
-		addl	%ebx %r9d
+		addl	%ebx, %r9d
 
 		# loop comparison
-		decl	%r15d
-		jnz		L1
+		incl	%r15d
+		cmpl	$32, %r15d
+		jb		L1_loop
 
 	# text[0] = y; text[1] = z;
 	movl	%r8d, 0(%rdi)
@@ -90,10 +91,88 @@ encrypt:
 
 
 decrypt:
-	
-						# %rdi points to buf
-						# %rsi points to key
-						
-						
+
+	# reserve the space for these registers
+	push	%r12
+	push	%r13
+	push	%r14
+	push	%r15
+
+	# %rdi points to buf
+	movl	0(%rdi), %r8d		# y (text[0]) 0-3 bytes
+	movl	4(%rdi), %r9d		# z (text[1]) 4-7 bytes
+
+	# %rsi points to key
+	movl	0(%rsi), %r10d	# k[0]	0-3 bytes
+	movl	4(%rsi), %r11d	# k[1]	4-7 bytes
+	movl	8(%rsi), %r12d	# k[2]	8-11 bytes
+	movl	12(%rsi), %r13d	# k[3]	11-15 bytes
+
+	# loop variables
+	movl	DELTA, %r14d		# total sum
+	shll	$5, %r14d				# delta << 5
+	movl	$0, %r15d				# n = 0
+
+	L2_loop:
+		# z -= ((y << 4) + k[2]) ^ (y+sum) ^ ((y >> 5) + k[3]);
+		# (y << 4) + k[2]
+		movl	%r8d, %ebx
+		shll	$4, %ebx
+		addl	%r12d, %ebx
+
+		# (y+sum)
+		movl	%r8d, %ecx
+		addl	%r14d, %ecx
+
+		xorl	%ecx, %ebx
+
+		# (y >> 5) + k[3]]
+		movl	%r8d, %edx
+		shrl	$5, %edx
+		addl	%r13d, %edx
+
+		xorl	%edx, %ebx
+
+		subl	%ebx, %r9d
+
+		# y -= ((z << 4) + k[0]) ^ (z+sum) ^ ((z >> 5) + k[1]);
+		# (z << 4) + k[0]
+		movl	%r9d, %ebx
+		shll	$4, %ebx
+		addl	%r10d, %ebx
+
+		# (z+sum)
+		movl	%r9d, %ecx
+		addl	%r14d, %ecx
+
+		xorl	%ecx, %ebx
+
+		# (z >> 5) + k[1]]
+		movl	%r9d, %edx
+		shrl	$5, %edx
+		addl	%r11d, %edx
+
+		xorl	%edx, %ebx
+
+		subl	%ebx, %r8d
+
+		# sum -= delta;
+		subl	DELTA, %r14d
+
+		# loop comparison
+		incl	%r15d
+		cmpl	$32, %r15d
+		jb		L2_loop
+
+	# text[0] = y; text[1] = z;
+	movl	%r8d, 0(%rdi)
+	movl	%r9d, 4(%rdi)
+
+	# free space for registers
+	pop		%r15
+	pop		%r14
+	pop		%r13
+	pop		%r12
+
 	retq
-	
+
